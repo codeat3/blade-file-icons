944f98035f02da5fdf0a4a7f0a24a6ff8b7c65f8
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2022-07-26 05:13:54 +0000

    Fix styling

--- src/BladeFileIconsServiceProvider.php
+++ src/BladeFileIconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeFileIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-file-icons', []);
 
-            $factory->add('file-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('file-icons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-file-icons.php', 'blade-file-icons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-file-icons.php', 'blade-file-icons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-file-icons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-file-icons'),
             ], 'blade-fileicon'); // TODO: change this alias to `blade-file-icons` in next major release
 
             $this->publishes([
-                __DIR__.'/../config/blade-file-icons.php' => $this->app->configPath('blade-file-icons.php'),
+                __DIR__ . '/../config/blade-file-icons.php' => $this->app->configPath('blade-file-icons.php'),
             ], 'blade-file-icons-config');
         }
     }
